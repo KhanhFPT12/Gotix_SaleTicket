4686fa035006979bb6f6222dfa522f9683d5b1af
KhanhFPT12 <khanhrey1803@gmail.com>
2026-05-30 18:38:53 +0700

    remove page transition ticket animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import AppRoutes from "./routes/AppRoutes";
 import AIChatBot from "./components/common/AIChatBot";
 import ScrollToTop from "./components/common/ScrollToTop";
-import PageTransition from "./components/common/PageTransition";
 
 export default function App() {
   return (
     <>
       <ScrollToTop />
-      <PageTransition />
       <AppRoutes />
       <AIChatBot />
     </>
